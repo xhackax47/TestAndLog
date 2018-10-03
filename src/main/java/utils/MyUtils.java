a2b1ffcc2f5e57173127f2d2c783bad42db02bdf
Samy Chaabi <xhackax47@gmail.com>
2018-10-03 16:18:29 +0200

    Issues Fixs

--- src/main/java/utils/MyUtils.java
+++ src/main/java/utils/MyUtils.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MyUtils {
-	
+
 	public static boolean compare(int[] tab1, int[] tab2) {
 
 		System.out.println("Voici  le tableau 1 : " + tab1);
@@ -31,58 +31,59 @@ public class MyUtils {
 			if (!al1.contains(i)) {
 				return false;
 			}
-			;
 		}
+		
 		for (int i : tab2) {
 			if (!al2.contains(i)) {
 				return false;
 			}
-			
+
 		}
+		
 		return true;
-}
-	
+	}
+
 	public static int sum(List<Integer> list) {
 		int i1 = 0;
-		for(int i2:list) {
-			i1+=i2;
+		for (int i2 : list) {
+			i1 += i2;
 		}
 		return i1;
-		
+
 	}
-	
+
 	public static String emptyToNull(String s) {
-		if(s == null) {
+		if (s == null) {
 			return null;
-		}else {
+		} else {
 			return s;
 		}
 	}
-	
+
 	public static List<Integer> filterEvenNumbers(List<Integer> integers) {
 		List<Integer> li = new ArrayList<Integer>();
-		
+
 //		for(int nb:integers) {
 //			if(nb % 2 == 0) {
 //				li.add(nb);
 //			}
 //		}
-		
-		li = integers.stream().filter(nb-> nb % 2 == 0).collect(Collectors.toList());
-		
+
+		li = integers.stream().filter(nb -> nb % 2 == 0).collect(Collectors.toList());
+
 		return li;
-		
+
 	}
-	
+
 	public static List<Float> transformSquarePlusPointFive(List<Float> floats) {
 		List<Float> lf = new ArrayList<Float>();
-		
+
 //		for(float nb:floats) {
 //			lf.add(nb * nb + 0.5F);
 //		}
-		
-		lf = floats.stream().map(x-> x*x+0.5F).collect(Collectors.toList());
-		
+
+		lf = floats.stream().map(x -> x * x + 0.5F).collect(Collectors.toList());
+
 		return lf;
 	}
 
